3c1157ac41325c894246563b5d7b90909fe0ca3b
Y Sruthi <ysruthi2020@gmail.com>
2022-05-17 20:32:32 +0530

    remove comment code in second component

--- src/app/taskthree/second/second.component.ts
+++ src/app/taskthree/second/second.component.ts
@@ -20,39 +20,17 @@ export class SecondComponent implements OnInit {
   @Output() enterValue = new EventEmitter<any>();
   startTimer() {
     this.nextStep.emit();
-    // this.enterValue.emit(this.timeLeft)
   }
 
 
-  // startTimer() {
-  //   this.startPause = !this.startPause;
-  //   if (this.startPause == false) {
-  //     this.startCount++
-  //     this.listOftimeToDisplay.push("Started At" + new Date())
-  //     this.interval = setInterval(() => {
-  //       if (this.timeLeft1 > 0) {
-  //         this.timeLeft1--;
-  //       } else {
-  //         this.timeLeft1 = 60;
-  //       }
-  //     }, 1000)
-  //   } else if (this.startPause == true) {
-  //    
-  //     this.pausedArray.push(this.timeLeft1)
-  //     this.pauseCount++
-  //     this.listOftimeToDisplay.push("paused At" + new Date())
-  //     this.pauseTimer();
-  //   }
- 
 
-  // }
   EnterTimer() {
     this.enterValue.emit(this.timeLeft1)
   }
   resetTimer() {
     this.timeLeft1 = 0;
     this.clearTimer.emit()
-    // clearInterval(this.interval);
+
   }
   constructor() { }
 
